26b9025e673db89f7792f83f1fd7fc56e7fcc416
g18932 <g18932is@gm.tsuda.ac.jp>
2021-09-09 12:44:19 +0900

    “userモデルをつくりました”

--- 4project/myApp/forms.py
+++ 4project/myApp/forms.py
@@ -2,44 +2,57 @@ from django import forms
 from .models import login
 from django.contrib.auth.forms import AuthenticationForm
 from django.forms import ModelForm
+from django.contrib.auth.models import User
 
 class UserForm(forms.ModelForm):
-   class Meta:
-       model = login
-       fields = (
-          'picture',
-          'name',
-          'password',
-          'mail',
-          'birth',
-          'school_name',
-          'school_grade',
-          'sexuality',
-          'insta_ID',
-          'twitter_ID',
-          'SNS_name1',
-          'SNS_ID1',
-          'SNS_name2',
-          'SNS_ID2',
-       )
-       labels = {
-          'picture':'プロフィール画像',
-          'name':'名前*',
-          'password':'パスワード*',
-          'mail':'メールアドレス*',
-          'birth':'生年月日*',
-          'school_name':'学校名*',
-          'school_grade':'学年*',
-          'sexuality':'性別*',
-          'insta_ID':'InstagramのID',
-          'twitter_ID':'TwitterのID',
-          'SNS_name1':'その他SNSの種類',
-          'SNS_ID1':'その他SNSのID',
-          'SNS_name2':'その他SNSの種類',
-          'SNS_ID2':'その他SNSのID',
-       }
-      
-class LoginForm(ModelForm):
-   class Meta:
+   password = forms.CharField(widget=forms.PasswordInput(),label="パスワード")
+   class Meta():
+      model = User
+      fields = (
+         'username',
+         'email',
+         'password',
+      )
+      labels = {
+         'username':'ユーザー名*',
+         'email':'メールアドレス*',
+         'password':'パスワード*',
+      }
+
+class AddUserForm(forms.ModelForm):
+   class Meta():
       model = login
-      fields = ['mail','password']
+      fields = (
+         'picture',
+         'last_name',
+         'first_name',
+         'birth',
+         'school_name',
+         'school_grade',
+         'sexuality',
+         'insta_ID',
+         'twitter_ID',
+         'SNS_name1',
+         'SNS_ID1',
+         'SNS_name2',
+         'SNS_ID2',
+      )
+      labels = {
+         'picture':'プロフィール画像',
+         'last_name':'名字',
+         'first_name':'名前',
+         'birth':'生年月日*',
+         'school_name':'学校名*',
+         'school_grade':'学年*',
+         'sexuality':'性別*',
+         'insta_ID':'InstagramのID',
+         'twitter_ID':'TwitterのID',
+         'SNS_name1':'その他SNSの種類',
+         'SNS_ID1':'その他SNSのID',
+         'SNS_name2':'その他SNSの種類',
+         'SNS_ID2':'その他SNSのID',
+      }
+
+class LoginForm(forms.Form):
+    username = forms.CharField()
+    password = forms.CharField()
--- 4project/myApp/models.py
+++ 4project/myApp/models.py
@@ -1,14 +1,18 @@
 from django.conf import settings
 from django.db import models
+from django.contrib.auth.models import User  
 
 class login(models.Model):
+
+   user = models.OneToOneField(User, on_delete=models.CASCADE)
+   
    picture = models.ImageField(upload_to='images/', default='images/initial.jpeg')
-   name = models.CharField(max_length=10) #自分の名前（本名)
-   password = models.CharField(max_length=8) #パスワード
-   mail = models.EmailField(max_length=240) #メールアドレス
+   last_name = models.CharField(max_length=100) #自分の名前（本名)
+   first_name = models.CharField(max_length=100)
+  
    birth = models.CharField(max_length=8) #生年月日
    school_name = models.CharField(max_length=50) #大学名
-   school_grade = models.CharField(max_length=1, choices=[('1','1年生'),('2', '2年生'),('3','3年生'), ('4','4年生'),('大学院','大学院生')]) #学年
+   school_grade = models.CharField(max_length=1, choices=[('1','1年生'),('2', '2年生'),('3','3年生'), ('4','4年生'),('5','大学院生')]) #学年
    sexuality = models.CharField(max_length=5, choices=[('男','男'),('女', '女'),('その他','その他')])#性別
    insta_ID = models.CharField(max_length=50, null=True, blank=True) #インスタのアカウント名
    twitter_ID = models.CharField(max_length=50, null=True, blank=True) #Twitterのアカウント名
@@ -17,11 +21,9 @@ class login(models.Model):
    SNS_name2 = models.CharField(max_length=50, null=True, blank=True) #その他SNSの種類
    SNS_ID2 = models.CharField(max_length=50, null=True, blank=True) #その他SNSのアカウント名
 
-   def publish(self):
-        self.save()
-
    def __str__(self):
-        return self.name
+        return self.user.username
+
 
 class hobby(models.Model):
 
