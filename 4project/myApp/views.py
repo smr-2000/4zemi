7e60c68bc42100c3379e7d67c2c41bff55d91dc9
smr-2000 <g18929ns@gm.tsuda.ac.jp>
2021-09-28 15:27:58 +0900

    趣味選択画面の追加

--- 4project/myApp/views.py
+++ 4project/myApp/views.py
@@ -263,8 +263,8 @@ def addSelectHobby(request,id):
 
 def showUpdateSelectHobby(request,id):
     userinfoHobby = get_object_or_404(hobby, pk=id)
-    favorite_hobby = SelectHobby(instance=userinfoHobby)
-
+    favorite_hobby = SelectHobby(instance=userinfoHobby)       
+    
     context = {
         'userinfoHobby':userinfoHobby,
         'favorite_hobby':favorite_hobby,
